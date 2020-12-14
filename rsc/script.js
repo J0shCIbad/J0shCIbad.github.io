f90f62f8e94dca728b8f51d22def290acaaedce0
Josh Come Ibad <joshcibad@gmail.com>
2020-12-14 11:39:14 -0800

    Update template format

--- rsc/script.js
+++ rsc/script.js
@@ -1,3 +1,16 @@
+/**
+ * Author: 	Josh Ibad
+ * Contact: joshcibad@gmail.com
+ * Project: Portfolio Website
+ * Version: v0.0.02 (2020-Dec-14)
+ * Filename: rsc/script.js
+ * Description: Main javascript file used by almost all pages in the website.
+ */
+
+
+/**
+ *	Toggles the visibility of the navigation bar/menu.
+ */
 function showMenuBar(){
 	if(document.getElementById("hor-nav-bar").style.visibility == "hidden"){
 		document.getElementById("hor-nav-bar").style.visibility = "visible";
