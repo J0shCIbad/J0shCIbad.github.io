c917dbe456b420f0e1a05f3d6f47caf93cec7f02
Josh Come Ibad <joshcibad@gmail.com>
2020-12-17 00:40:11 -0800

    Flexible navigation bar script

--- rsc/script.js
+++ rsc/script.js
@@ -7,6 +7,21 @@
  * Description: Main javascript file used by almost all pages in the website.
  */
 
+/**
+ * Generates the navigation bar
+ */
+function generateNavBar(opt){
+	var tmp_navBar = document.getElementById("hor-nav-bar");
+	tmp_navBar.insertAdjacentHTML("afterbegin", 
+		"<ul>\n" + 
+			"<li><a" + (opt===0 ? ' class="active" ' : " ") + 'href="/">Home</a></li>\n' +
+			"<li><a" + (opt===1 ? ' class="active" ' : " ") + 'href="/projects/">Projects</a></li>\n' +
+			"<li><a" + (opt===2 ? ' class="active" ' : " ") + 'href="/article/">Articles</a></li>\n' +
+			"<li><a" + (opt===3 ? ' class="active" ' : " ") + 'href="/tools/">Tools</a></li>\n' +
+			"<li><a" + (opt===4 ? ' class="active" ' : " ") + 'href="/contactme.html">Contact Me</a></li>\n' +
+		"</ul>\n"
+	);
+}
 
 /**
  *	Toggles the visibility of the navigation bar/menu.
